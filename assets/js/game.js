6c18789c362cc3c26d377958fc5b8fc8d6102643
Lawrence N. Masullo <lmasullo@gmail.com>
2019-06-10 11:32:05 -0500

    added js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-console.log("Connected!");
+$(document).ready(function(){
+  // $("p").click(function(){
+  //   $(this).hide();
+  // });
+
+  console.log("Connected!");
+  let randNum = Math.floor((Math.random() * 120) + 19);
+  let randNumCrystal = Math.floor((Math.random() * 12) + 1);
+
+  $("#random").text(randNum);
+
+});
+
+
+
+
