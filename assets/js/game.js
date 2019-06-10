b56557fd7ec64a13119d4fcfe37172cedba247a6
Larry Masullo <lmasullo@gmail.com>
2019-06-10 17:02:49 -0500

    Final Version

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,74 +3,6 @@ $(document).ready(() => {
   // Make sure we are connected
   console.log('Connected!');
 
-  //Reset the game
-  function reset(){
-    // Get the initial random numbers
-    randRet = rand();
-
-    //Reset the total score
-    let totScore = 0;
-    $('#scoreVal').text(totScore);
-  }
-
-  // Get the random numbers
-  function rand() {
-    // Get the random numbers, add them to an object and return
-    const objRand = {
-      randNum: Math.floor(Math.random() * 120 + 19),
-      randNumBlue: Math.floor(Math.random() * 12 + 1),
-      randNumGreen: Math.floor(Math.random() * 12 + 1),
-      randNumYellow: Math.floor(Math.random() * 12 + 1),
-      randNumRed: Math.floor(Math.random() * 12 + 1),
-    };
-    // console.log(objRand);
-
-    // Put the game's random number in the div
-    $('#random').text(objRand.randNum);
-
-    return objRand;
-  }//End rand
-
-  function incScore(value) {
-    totScore += value;
-    //console.log(totScore);
-
-    // Put the cumulative total score in the div
-    $('#scoreVal').text(totScore);
-
-    //Convert to a number
-    let scoreInt = Number($('#scoreVal').text());
-
-    console.log(scoreInt);
-
-    if (scoreInt === randRet.randNum) {
-      console.log("Win!!!!!!");
-      //Display Win Message
-      $('#message').text('You Win!!');
-
-      //Increment the wins and display
-      wins = wins += 1;
-      $('#wins').text(wins);
-
-      //Reset the game
-      reset();
-
-    }else if (scoreInt > randRet.randNum){
-      console.log("Lose!!!!!!");
-
-      //Display Loss Message
-      $('#message').text('You Lose!!');
-
-      //Increment the losses and display
-      losses = losses += 1;
-      $('#losses').text(losses);
-
-      //Reset the game
-      reset();
-
-    }//End check score against random number
-  }//End intScore
-
   // Set the initial score
   let totScore = 0;
   $('#scoreVal').text(totScore);
@@ -86,13 +18,81 @@ $(document).ready(() => {
   // Get the initial random numbers
   let randRet = rand();
 
+  // Reset the game
+  function reset() {
+    // Get the initial random numbers
+    randRet = rand();
+
+    // Reset the total score
+    totScore = 0;
+    $('#scoreVal').text(totScore);
+  }
+
+  // Get the random numbers
+  function rand() {
+    // Get the random numbers, add them to an object and return
+    const objRand = {
+      randNum: Math.floor(Math.random() * 120 + 19),
+      randNumBlue: Math.floor(Math.random() * 12 + 1),
+      randNumGreen: Math.floor(Math.random() * 12 + 1),
+      randNumYellow: Math.floor(Math.random() * 12 + 1),
+      randNumRed: Math.floor(Math.random() * 12 + 1),
+    };
+
+    // Put the game's random number in the div
+    $('#random').text(objRand.randNum);
+
+    // Retun all the random numbers in an object to use later
+    return objRand;
+  } // End rand
+
+  // Increment the score after each click
+  function incScore(value) {
+    totScore += value;
+    console.log(`Tot score: ${totScore}`);
+
+    // Put the cumulative total score in the div
+    $('#scoreVal').text(totScore);
+
+    // Convert to a number
+    const scoreInt = Number($('#scoreVal').text());
+
+    console.log(scoreInt);
+
+    // Compare the score with the random number
+    if (scoreInt === randRet.randNum) {
+      console.log('Win!!!!!!');
+      // Display Win Message
+      $('#message').text('You Win!!');
+
+      // Increment the wins and display
+      wins = wins += 1;
+      $('#wins').text(wins);
+
+      // Reset the game
+      reset();
+    } else if (scoreInt > randRet.randNum) {
+      console.log('Lose!!!!!!');
+
+      // Display Loss Message
+      $('#message').text('You Lose!!');
+
+      // Increment the losses and display
+      losses = losses += 1;
+      $('#losses').text(losses);
+
+      // Reset the game
+      reset();
+    } // End check score against random number
+  } // End intScore
+
   // Click the blue crystal
   $('#blue').click(() => {
     console.log(`Blue's value is: ${randRet.randNumBlue}`);
     incScore(randRet.randNumBlue);
   });
 
-  // Click the gree crystal
+  // Click the green crystal
   $('#green').click(() => {
     console.log(`Green's value is: ${randRet.randNumGreen}`);
     incScore(randRet.randNumGreen);
