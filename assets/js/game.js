c8e90cb39102bb432170e18bc4bd0d6aeb5c2623
Lawrence N. Masullo <lmasullo@gmail.com>
2019-06-10 14:08:46 -0500

    First complete working version

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,6 +3,16 @@ $(document).ready(() => {
   // Make sure we are connected
   console.log('Connected!');
 
+  //Reset the game
+  function reset(){
+    // Get the initial random numbers
+    randRet = rand();
+
+    //Reset the total score
+    let totScore = 0;
+    $('#scoreVal').text(totScore);
+  }
+
   // Get the random numbers
   function rand() {
     // Get the random numbers, add them to an object and return
@@ -14,26 +24,67 @@ $(document).ready(() => {
       randNumRed: Math.floor(Math.random() * 12 + 1),
     };
     // console.log(objRand);
+
+    // Put the game's random number in the div
+    $('#random').text(objRand.randNum);
+
     return objRand;
-  }
+  }//End rand
 
   function incScore(value) {
     totScore += value;
-    console.log(totScore);
+    //console.log(totScore);
 
-    return totScore;
-  }
+    // Put the cumulative total score in the div
+    $('#scoreVal').text(totScore);
+
+    //Convert to a number
+    let scoreInt = Number($('#scoreVal').text());
+
+    console.log(scoreInt);
+
+    if (scoreInt === randRet.randNum) {
+      console.log("Win!!!!!!");
+      //Display Win Message
+      $('#message').text('You Win!!');
+
+      //Increment the wins and display
+      wins = wins += 1;
+      $('#wins').text(wins);
+
+      //Reset the game
+      reset();
+
+    }else if (scoreInt > randRet.randNum){
+      console.log("Lose!!!!!!");
+
+      //Display Loss Message
+      $('#message').text('You Lose!!');
+
+      //Increment the losses and display
+      losses = losses += 1;
+      $('#losses').text(losses);
+
+      //Reset the game
+      reset();
+
+    }//End check score against random number
+  }//End intScore
 
   // Set the initial score
   let totScore = 0;
+  $('#scoreVal').text(totScore);
+
+  // Set the initial wins
+  let wins = 0;
+  $('#wins').text(wins);
+
+  // Set the initial losses
+  let losses = 0;
+  $('#losses').text(losses);
 
   // Get the initial random numbers
-  const randRet = rand();
-
-  console.log(randRet);
-
-  // Put the game's random number in the div
-  $('#random').text(randRet.randNum);
+  let randRet = rand();
 
   // Click the blue crystal
   $('#blue').click(() => {
