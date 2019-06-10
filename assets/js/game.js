d5c06c009a58acbf0ecd7e416e762b8089a633b1
Larry Masullo <lmasullo@gmail.com>
2019-06-10 12:03:26 -0500

    on laptop

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,12 @@
-$(document).ready(function(){
-  // $("p").click(function(){
-  //   $(this).hide();
-  // });
+// Make sure the document is loaded before running jQuery
+$(document).ready(() => {
+  // Make sure we are connected
+  console.log('Connected!');
+  const randNum = Math.floor(Math.random() * 120 + 19);
+  const randNumBlue = Math.floor(Math.random() * 12 + 1);
+  const randNumGreen = Math.floor(Math.random() * 12 + 1);
+  const randNumYellow = Math.floor(Math.random() * 12 + 1);
+  const randNumRed = Math.floor(Math.random() * 12 + 1);
 
-  console.log("Connected!");
-  let randNum = Math.floor((Math.random() * 120) + 19);
-  let randNumCrystal = Math.floor((Math.random() * 12) + 1);
-
-  $("#random").text(randNum);
-
+  $('#random').text(randNum);
 });
-
-
-
-
